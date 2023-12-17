035b30c96db804c78b99da452707fb01bb3cbde8
Josiah <47837306+josiahvehrs@users.noreply.github.com>
2023-12-16 18:47:14 -0600

    added array methods, also implemented for strings

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -10,11 +10,106 @@ var builtins = map[string]*object.Builtin{
 			}
 
 			switch arg := args[0].(type) {
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
 			default:
 				return newError("argument to `len` not supported, got %s", args[0].Type())
 			}
+		},
+	},
+	"first": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				if len(arg.Elements) > 0 {
+					return arg.Elements[0]
+				}
+				return NULL
+			case *object.String:
+				if len(arg.Value) > 0 {
+					return &object.String{Value: string(arg.Value[0])}
+				}
+				return NULL
+			default:
+				return newError("argument to `first` not supported, got %s, must be ARRAY or STRING", args[0].Type())
+			}
+		},
+	},
+	"last": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				length := len(arg.Elements)
+				if length > 0 {
+					return arg.Elements[length-1]
+				}
+				return NULL
+			case *object.String:
+				length := len(arg.Value)
+				if length > 0 {
+					return &object.String{Value: string(arg.Value[length-1])}
+				}
+				return NULL
+			default:
+				return newError("argument to `first` not supported, got %s, must be ARRAY or STRING", args[0].Type())
+			}
+		},
+	},
+	"rest": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				length := len(arg.Elements)
+				if length > 0 {
+					newElements := make([]object.Object, length-1)
+					copy(newElements, arg.Elements[1:length])
+					return &object.Array{Elements: newElements}
+				}
+				return NULL
+			case *object.String:
+				length := len(arg.Value)
+				if length > 0 {
+					newString := make([]byte, length-1)
+					copy(newString, []byte(arg.Value[1:length]))
+					return &object.String{Value: string(newString)}
+				}
+				return NULL
+			default:
+				return newError("argument to `first` not supported, got %s, must be ARRAY or STRING", args[0].Type())
+			}
+		},
+	},
+	"push": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `push` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+
+			newElements := make([]object.Object, length+1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1]
+
+			return &object.Array{Elements: newElements}
 		},
 	},
 }
